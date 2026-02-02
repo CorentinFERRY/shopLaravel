4b451b4ebebac068c28ee8a90a58ab5cdc8684c7
Corentin FERRY <c.ferry78@laposte.net>
2026-02-02 12:27:35 +0100

    Jour - 1 Bonus
    Bonus 1 & 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
@@ -8,11 +9,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function (){
+Route::get('/hello', function () {
     return "Hello Laravel !";
 });
 
 // Routes exercices 1 à 4 (nommés)
-Route::get('/home', [PageController::class ,'home'])->name('home'); // Renvois vers le produit 42
-Route::get('/about',[PageController::class,'about'])->name('about');
-Route::get('/products/{id}',[ProductController::class,'show'])->name('products.show');
+Route::get('/home', [PageController::class, 'home'])
+    ->name('home'); // nomme la route 
+Route::get('/about', [PageController::class, 'about'])
+    ->name('about');
+Route::get('/products/{id}', [ProductController::class, 'show'])
+    ->name('products.show')
+    ->where('id','[0-9]+');         // Ajoute une condition sur l'id d'être un nombre entier
+//  ->whereNumber('id');
+
+// Groupe de routes admin 
+// Crée un groupe pour le préfix '/admin' nommé par 'admin.xxxxx'
+Route::name('admin.')->prefix('/admin')->group(function () {            
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])
+        ->name('dashboard');                                                  // Ici on aura admin.dashboard 
+    Route::get('/listUsers', [AdminController::class, 'listUsers'])
+        ->name('listUsers');                                                  // Ici on aura admin.listUsers
+});
